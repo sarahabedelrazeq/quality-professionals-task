042a55f4ba09074f1822a5a5e138ace2d5614664
Sarah Abed Elrazeq <Sara.AbedAlrazeq@WARBABANK.COM>
2023-11-13 16:15:39 +0300

    Welcome  page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        'primary': '#043',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
