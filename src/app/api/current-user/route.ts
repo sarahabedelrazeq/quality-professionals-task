2326c4d10ce8c16d9a0978fa860c5a20220d1825
Sarah Abed Elrazeq <Sara.AbedAlrazeq@WARBABANK.COM>
2024-01-18 15:59:13 +0300

    current user API status

--- src/app/api/current-user/route.ts
+++ src/app/api/current-user/route.ts
@@ -3,8 +3,8 @@ import { promises as fs } from 'fs';
 export const GET = async () => {
     try {
         const file = await fs.readFile(process.cwd() + '/public/data/current-user.json', 'utf8');
-        return new Response(JSON.stringify({ data: JSON.parse(file), status: 200 }))
+        return new Response(JSON.stringify({ data: JSON.parse(file), status: 200 }), { status: 200 })
     } catch (error: any) {
-        return new Response(JSON.stringify({ error: error?.message, status: 500 }))
+        return new Response(JSON.stringify({ error: error?.message, status: 500 }), { status: 500 })
     }
 }
