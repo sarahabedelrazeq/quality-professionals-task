c68327d5467e04b1287a76ba411107e74a63d831
Sarah Abed Elrazeq <Sara.AbedAlrazeq@WARBABANK.COM>
2024-01-18 15:56:41 +0300

    rewrite the API

--- src/app/api/workspaces/route.ts
+++ src/app/api/workspaces/route.ts
@@ -2,9 +2,9 @@ import { promises as fs } from 'fs';
 
 export const GET = async () => {
     try {
-        // const file = await fs.readFile(process.cwd() + '/public/data/workspaces.json', 'utf8');
+        const file = await fs.readFile(process.cwd() + '/public/data/workspaces.json', 'utf8');
 
-        return new Response(process.cwd())
+        return new Response(JSON.stringify({ data: JSON.parse(file), status: 200 }))
     } catch (error: any) {
         return new Response(error, { status: 500 })
     }
@@ -19,8 +19,8 @@ export const POST = async (request: any) => {
             fs.writeFile(process.cwd() + '/public/data/workspaces.json', JSON.stringify([...JSON.parse(file), body]));
             return new Response(JSON.stringify({ status: 200 }), { status: 200 })
         }
-        return new Response(JSON.stringify({ message: 'You should add the workspace name and add at least one user', status: 400 }), { status: 400 })
+        return new Response(JSON.stringify({ message: 'You should add the workspace name and add at least one user', status: 400 }), { status: 200 })
     } catch (error: any) {
-        return new Response(JSON.stringify({ error: error?.message, status: 500 }), { status: 500 })
+        return new Response(JSON.stringify({ error: error?.message, status: 500 }), { status: 200 })
     }
 }
